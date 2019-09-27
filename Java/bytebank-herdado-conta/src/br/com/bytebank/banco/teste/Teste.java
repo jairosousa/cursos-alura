3435ac95f36a4b4650304f13d7b29e8cf5169cfd
jaironsousa1 <jaironsousa@gmail.com>
2019-09-27 07:44:10 -0300

    Conhecendo Java.util 02 - conhecendo Arrays

--- Java/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
+++ Java/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
@@ -6,32 +6,25 @@ import br.com.bytebank.banco.modelo.ContaPoupanca;
 public class Teste {
 
 	public static void main(String[] args) {
-
-//		System.out.println("x");
-//        System.out.println(3);
-//        System.out.println(false);
-        
-        ContaCorrente cc = new ContaCorrente(22, 33);
-        ContaPoupanca cp = new ContaPoupanca(33, 22);	
-        
-        System.out.println(cc);
-        System.out.println(cp);
-        
-//        println(cc);
-
+		
+		int[] idades = new int[5];//Já inicializa com valores padrões.
+		
+		System.out.println(idades[0]);
+		
+//		idades[0] = 29;
+//		idades[1] = 39;
+//		idades[2] = 49;
+//		idades[3] = 59;
+//		idades[4] = 69;
+//		
+//		System.out.println(idades.length);
+		
+		for (int i = 0; i < idades.length; i++) {
+			idades[i] = i * i;
+		}
+		
+		for (int i = 0; i < idades.length; i++) {
+			System.out.println(idades[i]);
+		}
 	}
-	static void println() {
-    }
-
-    static void println(int a) {
-    }
-
-    static void println(boolean valor) {
-    }
-    
-    static void println(ContaCorrente conta) {
-    }
-    
-    static void println(Object referencia) {
-    }
 }
