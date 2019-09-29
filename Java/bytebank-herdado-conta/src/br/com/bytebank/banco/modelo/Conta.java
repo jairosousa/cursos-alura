995701f6a6e89b994dac8481d8064ab5ec6f6c6f
jaironsousa1 <jaironsousa@gmail.com>
2019-09-29 11:42:54 -0300

    04 - Equals e mais listas

--- Java/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ Java/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -96,4 +96,30 @@ public abstract class Conta {
 	public String toString() {
 		return "Numero: " + this.numero + ", Agencia:" + this.agencia;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + agencia;
+		result = prime * result + numero;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		if (agencia != other.agencia)
+			return false;
+		if (numero != other.numero)
+			return false;
+		return true;
+	}
+
 }
--- Java/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/TesteArrayList.java
+++ Java/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/TesteArrayList.java
@@ -1,17 +1,18 @@
 package br.com.bytebank.banco.teste.util;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import br.com.bytebank.banco.modelo.Cliente;
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 
-public class Teste {
+public class TesteArrayList {
 
 	public static void main(String[] args) {
 
 		// Generics
-		ArrayList<Conta> lista = new ArrayList();
+		List<Conta> lista = new ArrayList();
 		
 		Cliente c = new Cliente();
 //		lista.add(c); nem compila
