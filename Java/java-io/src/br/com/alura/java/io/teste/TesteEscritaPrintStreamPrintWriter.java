e30248f01f1ccbdafb2d767291c9ae1153a70672
Jairo Nascimento <jaironsousa@gmail.com>
2019-10-01 17:54:58 -0300

    04 - Usando java.util.Scanner/ 01 Leitura com Scanner

--- Java/java-io/src/br/com/alura/java/io/teste/TesteEscritaPrintStreamPrintWriter.java
+++ Java/java-io/src/br/com/alura/java/io/teste/TesteEscritaPrintStreamPrintWriter.java
@@ -1,10 +1,9 @@
 package br.com.alura.java.io.teste;
 
 import java.io.IOException;
-import java.io.PrintStream;
 import java.io.PrintWriter;
 
-public class TesteEscrita3 {
+public class TesteEscritaPrintStreamPrintWriter {
 
 	public static void main(String[] args) throws IOException {
 
