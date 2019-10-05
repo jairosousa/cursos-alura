dd00b7cfb500d4d2d0cff1bdb2476c90e54648b2
jaironsousa1 <jaironsousa@gmail.com>
2019-10-05 19:54:18 -0300

    06 - Aplicando o Set no modelo

--- Java/gerenciador-cursos/src/br/com/alura/Curso.java
+++ Java/gerenciador-cursos/src/br/com/alura/Curso.java
@@ -1,14 +1,17 @@
 package br.com.alura;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
 
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<>();
+	private Set<Aluno> alunos = new HashSet<>();
 
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -44,12 +47,19 @@ public class Curso {
 //		return tempoTotal;
 		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
 	}
+	
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
+	}
 
 	@Override
 	public String toString() {
-		return "Curso [nome=" + nome + ", instrutor=" + instrutor + ", Tempo Total=" + tempoTotal() + 
-				", Aulas: "+ aulas + "]";
+		return "Curso [nome=" + nome + ", instrutor=" + instrutor + ", Tempo Total=" + tempoTotal() + ", Aulas: "
+				+ aulas + "]";
 	}
-	
-	
+
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
+	}
+
 }
