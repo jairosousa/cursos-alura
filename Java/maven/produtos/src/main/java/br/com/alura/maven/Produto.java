df95caf00752c69571d943af24a25aa7134f91f0
jaironsousa1 <jaironsousa@gmail.com>
2019-10-06 13:28:23 -0300

    04 - Relatórios de qualidade e cobertura

--- Java/maven/produtos/src/main/java/br/com/alura/maven/Produto.java
+++ Java/maven/produtos/src/main/java/br/com/alura/maven/Produto.java
@@ -4,11 +4,21 @@ public class Produto {
 
 	private final String nome;
 	private final double preco;
-	
+
 	public Produto(String nome, double preco) {
 		this.nome = nome;
 		this.preco = preco;
 	}
-	
-	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public double getPrecoComImposto() {
+		return preco * 1.10;
+	}
 }
