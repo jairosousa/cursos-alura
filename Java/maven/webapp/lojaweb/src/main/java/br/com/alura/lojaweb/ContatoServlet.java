9718569773301c2e4ee978928aac98533ffcd6a4
jaironsousa1 <jaironsousa@gmail.com>
2019-10-06 15:26:32 -0300

    06 - Projeto web

--- Java/maven/webapp/lojaweb/src/main/java/br/com/alura/lojaweb/ContatoServlet.java
+++ Java/maven/webapp/lojaweb/src/main/java/br/com/alura/lojaweb/ContatoServlet.java
@@ -9,11 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.alura.maven.Produto;
+
 @WebServlet(urlPatterns = {"/contato"})
 public class ContatoServlet extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		Produto produto = new Produto("bala", 0.15);
 		PrintWriter writer = resp.getWriter();
 		writer.println("<html><h2>Fale conosco</h2></html>");
 		writer.close();
