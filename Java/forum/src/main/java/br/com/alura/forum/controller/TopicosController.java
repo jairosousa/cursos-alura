58814024bf8a90c4c6a00aa2074c528fd7cd016d
Jairo Nascimento <jaironsousa@gmail.com>
2019-10-07 13:18:14 -0300

    03 - Usando Spring Data

--- Java/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ Java/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,22 +1,26 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
-import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
+	
+	@Autowired
+	private TopicoRepository topicosRepository;
 
 	@RequestMapping("/topicos")
-	public List<TopicoDto> lista(){
-		Topico topico = new Topico("Dúvida", "Dúvida com String", new Curso("String", "Programação"));
-		
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico)); 
+	public List<TopicoDto> lista(String nomeCurso){
+		 if(nomeCurso == null) {
+			 return TopicoDto.converter(topicosRepository.findAll()); 
+		 } else {
+			 return TopicoDto.converter(topicosRepository.findByCursoNome(nomeCurso));
+		 }
 	}
 }
