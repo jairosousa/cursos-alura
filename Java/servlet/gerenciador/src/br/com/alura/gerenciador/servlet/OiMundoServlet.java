49c40721b720f014c5990cc7f716e93e7415b829
Jairo Nascimento <jaironsousa@gmail.com>
2019-10-12 16:18:05 -0300

    08 - Deploy da aplicação

--- Java/servlet/gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ Java/servlet/gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -8,8 +8,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/oi")
+@WebServlet(urlPatterns = "/oi", loadOnStartup=1)
 public class OiMundoServlet extends HttpServlet {
+
+	public OiMundoServlet() {
+		System.out.println("Criando Oi Mundo servlet");
+	}
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -20,7 +24,7 @@ public class OiMundoServlet extends HttpServlet {
 		out.println("<h1>Oi mundo!! Parabéns você escreveu o primeiro servlets</h1>");
 		out.println("</body>");
 		out.println("</html>");
-	
+
 		System.out.println("O Servelt OiMundoServlet foi chamdado");
 	}
 
