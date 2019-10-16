2931ac536eb41431c0e96773d7f93653e429bd18
Jairo Nascimento <jaironsousa@gmail.com>
2019-10-16 10:17:43 -0300

    02 - Melhorando desempenho com Spring Cache

--- Java/rest-spring-boot/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ Java/rest-spring-boot/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,15 +1,15 @@
 package br.com.alura.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
@@ -44,6 +44,7 @@ public class TopicosController {
 	private CursoRepository cursorepository;
 
 	@GetMapping
+	@Cacheable(value = "listaDeTopicos")
 	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
 			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 2) Pageable paginacao) {
 
@@ -61,6 +62,7 @@ public class TopicosController {
 
 	@PostMapping
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursorepository);
 		topicosRepository.save(topico);
@@ -80,6 +82,7 @@ public class TopicosController {
 
 	@PutMapping("/{id}")
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
 
 		Optional<Topico> optional = topicosRepository.findById(id);
@@ -93,6 +96,7 @@ public class TopicosController {
 
 	@DeleteMapping("/{id}")
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true) 
 	public ResponseEntity<?> remover(@PathVariable Long id) {
 
 		Optional<Topico> optional = topicosRepository.findById(id);
