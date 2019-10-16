4ccd956c4d45d521a6b01567d2315b5deee1b6e6
Jairo Nascimento <jaironsousa@gmail.com>
2019-10-16 11:37:39 -0300

    03 - Proteção com Spring Security

--- Java/rest-spring-boot/forum/src/main/resources/data.sql
+++ Java/rest-spring-boot/forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$Gq9owvor1Xd8OjaR7DfX9uvGiJgKNFYt7oCbL7kxrM5OnJwIUCAme');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
