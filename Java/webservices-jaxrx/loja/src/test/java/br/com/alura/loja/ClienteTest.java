5d71b633529ea1aff7499d129fad1a5168ce597b
Jairo Nascimento <jaironsousa@gmail.com>
2019-10-17 09:14:26 -0300

    02 - Testes end-to-end com JAX-RS e Grizzly

--- Java/webservices-jaxrx/loja/src/test/java/br/com/alura/loja/ClienteTest.java
+++ Java/webservices-jaxrx/loja/src/test/java/br/com/alura/loja/ClienteTest.java
@@ -3,13 +3,44 @@ package br.com.alura.loja;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
 
 public class ClienteTest {
 
+	private HttpServer server;
+
+	@Before
+	public void startServidor() {
+		server = Servidor.InicializaServidor();
+	}
+
+	@After
+	public void stopServidor() {
+		server.stop();
+	}
+
+	@Test
+	public void testeQueBuscaUmCarrinhoTrazOCarrinhoEsperado() {
+		
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		String conteudo = target.path("/carrinhos").request().get(String.class);
+		
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		
+		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+		
+	}
+	
 	@Test
 	public void testaQueAConexaoComOServidorFunciona() {
 		Client client = ClientBuilder.newClient();
@@ -19,5 +50,6 @@ public class ClienteTest {
 		Assert.assertTrue(conteudo.contains("Rua Vergueiro 3185"));
 
 	}
+
 
 }
