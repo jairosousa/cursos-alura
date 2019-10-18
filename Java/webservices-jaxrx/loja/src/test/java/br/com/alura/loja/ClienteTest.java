62ff098bab15814b1a89162e694289d13af08ee3
Jairo Nascimento <jaironsousa@gmail.com>
2019-10-18 16:22:25 -0300

    07 - Status code e a Interface Uniforme

--- Java/webservices-jaxrx/loja/src/test/java/br/com/alura/loja/ClienteTest.java
+++ Java/webservices-jaxrx/loja/src/test/java/br/com/alura/loja/ClienteTest.java
@@ -1,18 +1,22 @@
 package br.com.alura.loja;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.ws.http.HTTPBinding;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.sun.research.ws.wadl.HTTPMethods;
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.modelo.Carrinho;
@@ -21,10 +25,17 @@ import br.com.alura.loja.modelo.Produto;
 public class ClienteTest {
 
 	private HttpServer server;
+	
+	private Client client;
+
+	private WebTarget target;
 
 	@Before
 	public void startServidor() {
 		server = Servidor.InicializaServidor();
+		this.client = ClientBuilder.newClient();
+		this.target = client.target("http://localhost:8080");
+
 	}
 
 	@After
@@ -35,30 +46,25 @@ public class ClienteTest {
 	@Test
 	public void testeQueBuscaUmCarrinhoTrazOCarrinhoEsperado() {
 		
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080");
 		String conteudo = target.path("/carrinhos/1").request().get(String.class);
 		
 		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
 		
-		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+		assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
 		
 	}
 	
 	@Test
 	public void testaQueAConexaoComOServidorFunciona() {
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://www.mocky.io");
+		
 		String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
 
-		Assert.assertTrue(conteudo.contains("Rua Vergueiro 3185"));
+		assertTrue(conteudo.contains("Rua Vergueiro 3185"));
 
 	}
 	
 	@Test
 	public void testaAdicionarUmCarrinho() {
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080");
 		
 		// Criar Carrinho
 		Carrinho carrinho = new Carrinho();
@@ -74,7 +80,14 @@ public class ClienteTest {
         Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
 
         Response response = target.path("/carrinhos").request().post(entity);
-        Assert.assertEquals("<status>sucesso</status>", response.readEntity(String.class));
+        
+        assertEquals(201, response.getStatus());
+        
+        String location = response.getHeaderString("location");
+        
+        String conteudo = client.target(location).request().get(String.class);
+        
+        assertTrue(conteudo.contains("Tablet"));
 		
 	}
 
