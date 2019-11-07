d99d406d176cd86703dba2c4979c0d3e076bfebd
Jairo Nascimento <jaironsousa@gmail.com>
2019-11-07 14:25:02 -0300

    Servlet parte 2: 03 - Formulario Login

--- Java/servlet/gerenciador/src/br/com/alura/gerenciador/modelo/Banco.java
+++ Java/servlet/gerenciador/src/br/com/alura/gerenciador/modelo/Banco.java
@@ -8,17 +8,29 @@ import java.util.List;
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<>();
+	private static List<Usuario> listaUsuarios = new ArrayList<>();
 	private static Integer chaveSequencia = 1;
 
 	static {
 		Empresa empresa1 = new Empresa();
 		empresa1.setId(chaveSequencia++);
 		empresa1.setNome("Caelum");
+		
 		Empresa empresa2 = new Empresa();
 		empresa2.setId(chaveSequencia++);
 		empresa2.setNome("Alura");
 
 		Banco.lista.addAll(Arrays.asList(empresa1, empresa2));
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("jairo");
+		u1.setSenha("12345");
+		
+		Usuario u2 = new Usuario();
+		u2.setLogin("silvana");
+		u2.setSenha("12345");
+		
+		Banco.listaUsuarios.addAll(Arrays.asList(u1, u2));
 	}
 
 	public void adiciona(Empresa empresa) {
@@ -49,6 +61,15 @@ public class Banco {
 		for (Empresa empresa : lista) {
 			if (empresa.getId() == id) {
 				return empresa;
+			}
+		}
+		return null;
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		for(Usuario usuario : listaUsuarios) {
+			if (usuario.ehIgual(login, senha)) {
+				return usuario;
 			}
 		}
 		return null;
