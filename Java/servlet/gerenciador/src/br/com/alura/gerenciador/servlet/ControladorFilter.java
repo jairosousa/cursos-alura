d9d6accc38a5481c1c78b9d3935b782ba70109b6
Jairo Nascimento <jaironsousa@gmail.com>
2019-11-08 14:41:23 -0300

    Servlet 2 06 - Introdução ao web Service

--- Java/servlet/gerenciador/src/br/com/alura/gerenciador/servlet/ControladorFilter.java
+++ Java/servlet/gerenciador/src/br/com/alura/gerenciador/servlet/ControladorFilter.java
@@ -8,10 +8,8 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.acao.Acao;
 
