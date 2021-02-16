8c64423ebd547cc7cefb7cc81dc2d1aac42d17fd
Jairo <jaironsousa@gmail.com>
2021-02-16 17:52:38 -0300

    :construction: Criando projeto
    Inicio curso de padrões de projeto: Strategy

---
+++ Java/padroes/comportamentais/strategy/loja/src/br/com/alura/loja/orcamentoOrcamento.java
@@ -0,0 +1,16 @@
+package br.com.alura.loja;
+
+import java.math.BigDecimal;
+
+public class orcamentoOrcamento {
+
+    private BigDecimal valor;
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+}
