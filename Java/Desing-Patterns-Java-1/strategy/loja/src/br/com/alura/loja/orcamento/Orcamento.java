f399fcf91f465dc4a9cf520c843a6bd0080b19d4
Jairo <jaironsousa@gmail.com>
2021-05-30 15:29:10 -0300

    Adicionando a situação do orçamento

--- Java/Desing-Patterns-Java-1/strategy/loja/src/br/com/alura/loja/orcamento/Orcamento.java
+++ Java/Desing-Patterns-Java-1/strategy/loja/src/br/com/alura/loja/orcamento/Orcamento.java
@@ -10,11 +10,27 @@ public class Orcamento {
 
   private BigDecimal valor;
   private int quantidadeItens;
+  private String situacao;
 
   public Orcamento(BigDecimal valor, int quantidadeItens) {
     this.valor = valor;
     this.quantidadeItens = quantidadeItens;
   }
+
+  public void aplicarDescontoextra() {
+    BigDecimal valordescontoExtra = BigDecimal.ZERO;
+    if (situacao.equals("EM ANALISE")) {
+      valordescontoExtra = this.valor.multiply(new BigDecimal("0.05"));
+    } else if (situacao.equals("APROVADO")) {
+      valordescontoExtra = this.valor.multiply(new BigDecimal("0.02"));
+    }
+    this.valor = this.valor.subtract(valordescontoExtra);
+  }
+
+  public void aprovar() {
+    this.situacao = "APROVADO";
+  }
+
   public BigDecimal getValor() {
     return valor;
   }
