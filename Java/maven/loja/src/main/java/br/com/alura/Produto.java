7d45923a714eac3cb55eb02257269aec99d8f2e3
Jairo <jaironsousa@gmail.com>
2021-06-21 15:18:13 -0300

    :fire: Conclusão do curso maven

--- Java/maven/loja/src/main/java/br/com/alura/Produto.java
+++ Java/maven/loja/src/main/java/br/com/alura/Produto.java
@@ -4,6 +4,23 @@ package br.com.alura;/*
  *
  */
 
+import java.math.BigDecimal;
+
 public class Produto {
 
+  private String nome;
+  private BigDecimal valor;
+
+  public Produto(String nome, BigDecimal valor) {
+    this.nome = nome;
+    this.valor = valor;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public BigDecimal getValor() {
+    return valor;
+  }
 }
--- Java/maven/loja/src/test/java/br/com/alura/ProdutoTest.java
+++ Java/maven/loja/src/test/java/br/com/alura/ProdutoTest.java
@@ -6,6 +6,17 @@ package br.com.alura;
  *
  */
 
-class ProdutoTest {
+import java.math.BigDecimal;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProdutoTest {
+
+  @Test
+  public void testProduto() {
+    Produto produto = new Produto("Teste", BigDecimal.TEN);
+    Assert.assertEquals("Teste", produto.getNome());
+    Assert.assertEquals(BigDecimal.TEN, produto.getValor());
+  }
 
 }
