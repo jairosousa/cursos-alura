359ac588c24e7bd7e8163e40081e3e944c309945
Jairo <jaironsousa@gmail.com>
2021-06-27 12:37:22 -0300

    :trophy: curso Persistência com JPA: Introdução ao Hibernate :medal_military: Finalizando o módulo: 05 Consultas com jpql :medal_sports: Finalizando o Curso  - Como realizar consultas com o método find do Entity Manager;  - Como realizar consultas JPQL;  - Como filtrar dados em consultas JPQL;  - Como retornar apenas parte de uma entidade em uma consulta JPQL.

--- Java/formacao-java/jdbc-spring/java-jpa/jpa-hibernate-persistencia-projeto_inicial/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ Java/formacao-java/jdbc-spring/java-jpa/jpa-hibernate-persistencia-projeto_inicial/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -1,6 +1,8 @@
 package br.com.alura.loja.dao;
 
 import br.com.alura.loja.modelo.Produto;
+import java.math.BigDecimal;
+import java.util.List;
 import javax.persistence.EntityManager;
 
 /**
@@ -16,5 +18,35 @@ public class ProdutoDao {
 
   public void cadastrar(Produto produto) {
     this.em.persist(produto);
+  }
+
+  public Produto buscaPorId(Long id) {
+    return this.em.find(Produto.class, id);
+  }
+
+  public List<Produto> buscaTodos() {
+    String jpql = "SELECT p FROM Produto p";
+    return this.em.createQuery(jpql, Produto.class).getResultList();
+  }
+
+  public List<Produto> buscaPorNome(String nome) {
+    String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
+    return this.em.createQuery(jpql, Produto.class)
+        .setParameter("nome", nome)
+        .getResultList();
+  }
+
+  public List<Produto> buscaPorNomeDaCategoria(String nome) {
+    String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome";
+    return this.em.createQuery(jpql, Produto.class)
+        .setParameter("nome", nome)
+        .getResultList();
+  }
+
+  public BigDecimal buscarPrecoDoProdutoComNome(String nome) {
+    String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
+    return this.em.createQuery(jpql, BigDecimal.class)
+        .setParameter("nome", nome)
+        .getSingleResult();
   }
 }
--- Java/formacao-java/jdbc-spring/java-jpa/jpa-hibernate-persistencia-projeto_inicial/src/main/java/br/com/alura/loja/main/CadastroDeProdutos.java
+++ Java/formacao-java/jdbc-spring/java-jpa/jpa-hibernate-persistencia-projeto_inicial/src/main/java/br/com/alura/loja/main/CadastroDeProdutos.java
@@ -6,9 +6,8 @@ import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 import java.math.BigDecimal;
+import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 /**
  * @Author: Jairo Nascimento on 27/06/2021 - 09:11
@@ -17,10 +16,36 @@ public class CadastroDeProdutos {
 
   public static void main(String[] args) {
 
+    cadastroProduto();
+
+    EntityManager em = JPAUtil.getEntityManager();
+
+    ProdutoDao produtoDao = new ProdutoDao(em);
+    Produto produto = produtoDao.buscaPorId(1l);
+    System.out.println(produto.getNome() + "/" + produto.getPreco());
+
+    List<Produto> todos = produtoDao.buscaTodos();
+    todos.forEach(p1 -> System.out.println(p1.getNome()));
+
+    System.out.println("\n----Busca por nome----");
+    List<Produto> nomes = produtoDao.buscaPorNome("Xiaomi redmi");
+    nomes.forEach(p1 -> System.out.println(p1.getNome()));
+
+    System.out.println("\n----Busca por nome Categoria----");
+    List<Produto> nomesCategoria = produtoDao.buscaPorNomeDaCategoria("Celulares");
+    nomesCategoria.forEach(p1 -> System.out.println(p1.getNome()));
+
+    System.out.println("\n----Buscar Preco do Produto com Nome----");
+    System.out.println("PREÇO PRODUTO" + produtoDao.buscarPrecoDoProdutoComNome("Xiaomi redmi"));
+
+
+  }
+
+  private static void cadastroProduto() {
     Categoria celulares = new Categoria("Celulares");
 
     Produto celular =
-        new Produto("Xiaomi redmi","Muito legal",new BigDecimal("800"), celulares);
+        new Produto("Xiaomi redmi", "Muito legal", new BigDecimal("800"), celulares);
 
     EntityManager em = JPAUtil.getEntityManager();
 
