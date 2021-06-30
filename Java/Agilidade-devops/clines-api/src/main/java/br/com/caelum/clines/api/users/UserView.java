7b4b1bbce85b84be93a83a5db403719d4f052726
Jairo <jaironsousa@gmail.com>
2021-06-29 21:06:34 -0300

    :rocket: Curso de Agilidade e DevOps: Um dia no desenvolvimento de software :medal_military: Finalizando o Módulo 02 Containerizando a aplicação. :heavy_check_mark: As vantagens de se utilizar containers Docker;  :heavy_check_mark: Como configurar o Docker na aplicação;  :heavy_check_mark: Como criar uma imagem Docker da aplicação e executar um container dela;  :heavy_check_mark: Como utilizar o docker compose para simplificar a gestão dos containers utilizados pela aplicação.

---
+++ Java/Agilidade-devops/clines-api/src/main/java/br/com/caelum/clines/api/users/UserView.java
@@ -0,0 +1,11 @@
+package br.com.caelum.clines.api.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserView {
+    private String name;
+    private String email;
+}
