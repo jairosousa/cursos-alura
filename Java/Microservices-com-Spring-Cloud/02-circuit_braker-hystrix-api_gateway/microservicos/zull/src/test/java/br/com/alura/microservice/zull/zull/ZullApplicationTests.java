dc8013899ad2aaac44b9e020677804771561e8f6
Jairo <jaironsousa@gmail.com>
2021-08-08 11:22:43 -0300

    :rocket: Microservice com Spring Clound: Circuit Breaker, Hystrix a API Gateway :medal_military: Finalizando o Módulo 04 API Gateway com Spring Zuul.  :heavy_check_mark: Como disponibilizar a nossa aplicação para serem acessadas por um cliente;  :heavy_check_mark: O uso do Api Gateway, para criar um único ponto de acesso à nossa aplicação;  :heavy_check_mark: O uso da implementação Zuul como Api Gateway do nosso sistema;  :heavy_check_mark: A integração do Zuul com o Eureka, para o descobrimento automático das instâncias disponíveis da nossa aplicação;  :heavy_check_mark: A alteração das requisições para os nossos microsserviços, com o uso do Zuul.

---
+++ Java/Microservices-com-Spring-Cloud/02-circuit_braker-hystrix-api_gateway/microservicos/zull/src/test/java/br/com/alura/microservice/zull/zull/ZullApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.microservice.zull.zull;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
