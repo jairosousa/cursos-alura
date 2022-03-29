ff71d53e9f9c357a60224cae4c10ea4032b41c3f
Jairo <jaironsousa@gmail.com>
2022-03-29 10:45:01 -0300

    Criando chaves SSH - Como gerar um par de chaves SSH próprio utilizando o ssh-keygen. - Como criar o recurso de aws_key_pair para registrar a nossa chave na aws. - As informações necessárias para criar o aws_key_pair. - Como ler diretamente um arquivo, mantendo as chaves privadas.

---
+++ IAC/terraform/separando-ambientes/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = ">= 1.1.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+} 
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "aprendizado"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  key_name      = "aws-jairo"
+  security_groups = ["sgr-access-tf", "sgr-access-web-tf"]
+
+  tags = {
+    Name = "aws Ansible Python Terraform"
+  }
+}
+
+resource "aws_key_pair" "chaveSSH" {
+  key_name = DEV
+  public_key = file(IaC-DEV.pub)
+  
+}
